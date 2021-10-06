08ed6ed88c550309727c3959d35b22e33b46b795
brad kim <brad.kim@epiclions.net>
2021-10-06 15:20:18 +0900

    change

--- src/utils/callHelpers.ts
+++ src/utils/callHelpers.ts
@@ -1,6 +1,5 @@
 import BigNumber from 'bignumber.js'
 import { ethers } from 'ethers'
-import { getReferralCode } from 'utils/referrals'
 
 export const approve = async (lpContract, masterChefContract, account) => {
   return lpContract.methods
@@ -10,7 +9,7 @@ export const approve = async (lpContract, masterChefContract, account) => {
 
 export const stake = async (masterChefContract, pid, amount, account) => {
   return masterChefContract.methods
-    .deposit(pid, new BigNumber(amount).times(new BigNumber(10).pow(18)).toString(), getReferralCode())
+    .deposit(pid, new BigNumber(amount).times(new BigNumber(10).pow(18)).toString())
     .send({ from: account })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
@@ -81,7 +80,7 @@ export const sousEmegencyUnstake = async (sousChefContract, amount, account) => 
 
 export const harvest = async (masterChefContract, pid, account) => {
   return masterChefContract.methods
-    .deposit(pid, '0', getReferralCode())
+    .deposit(pid, '0')
     .send({ from: account })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
